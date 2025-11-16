32837bc18d7da8c1be0a3d74464c4b5431c8f84b
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:34:44 -0600

    Update pid.cpp
    Add file description

--- avionics/firmware/src/control/pid.cpp
+++ avionics/firmware/src/control/pid.cpp
@@ -1,1 +1,9 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Control: PID
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Generate PID structs and functions such as pidInit, pidReset, and pidUpdate.
+// Shall be used for roll, pitch, and yaw and attitude added when needed.
