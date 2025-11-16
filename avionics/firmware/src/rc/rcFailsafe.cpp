04d04cd235f463f2f1962c2230633a025a67f14a
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:17:32 -0600

    Update rcFailsafe.cpp
    Add file description

--- avionics/firmware/src/rc/rcFailsafe.cpp
+++ avionics/firmware/src/rc/rcFailsafe.cpp
@@ -1,1 +1,11 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware RC: Failsafe
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Lost link handling
+// If the link is lost:
+//    OPTION 1: Set surfaces to glide configuration and cut throttle
+//    OPTION 2: Gradually roll the plane level and slowly lower throttle
