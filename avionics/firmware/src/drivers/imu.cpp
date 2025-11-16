8779d9672b0fe7d76ee7cd464783cbe9ded27db0
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:25:15 -0600

    Update imu.cpp
    Add file description comments

--- avionics/firmware/src/drivers/imu.cpp
+++ avionics/firmware/src/drivers/imu.cpp
@@ -1,1 +1,8 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Drivers: IMU
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Low-level sensor initiation to calibrate gyro bias at startup and return ImuData (gyro, acceleration, timestamps) in struct form.
