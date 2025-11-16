5ec250b308044983bacedc934b82b4fac01f97af
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:32:58 -0600

    Update rfLink.cpp
    Add file description

--- controller/firmware/src/drivers/rfLink.cpp
+++ controller/firmware/src/drivers/rfLink.cpp
@@ -1,1 +1,9 @@
+// Tropochief Project
+// Chris Dillow
+// Controller Firmware Drivers: RF Link
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Send frames over RF reliably
+// OPTIONAL: Improve testing and performance with implementation of replies/ack
