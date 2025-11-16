50f2e9e92fb66b6efc35e5718098c31f36bbb867
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:31:01 -0600

    Update attitudeEstimator.cpp
    Add file description

--- avionics/firmware/src/control/attitudeEstimator.cpp
+++ avionics/firmware/src/control/attitudeEstimator.cpp
@@ -1,1 +1,11 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Control: Attitude Estimator
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Should implement choice of Mahony or Madgwick filter.
+// Takes inputs from gyroscope, accelerometer, dt
+// Shall output quarternion and/or Euler angles (roll, pitch, and yaw)
+// OPTIONAL: If desired, provide a gravity-compensated acceleration estimate.
