9265331b0975439efdd0bfe6f9f06ca14d83348e
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:31:19 -0600

    Update rcFrame.cpp
    Add file description

--- controller/firmware/src/rcFrame.cpp
+++ controller/firmware/src/rcFrame.cpp
@@ -1,1 +1,8 @@
+// Tropochief Project
+// Chris Dillow
+// Controller Firmware: RC Frame
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Pack to a binary frame with a simple header and checksum.
