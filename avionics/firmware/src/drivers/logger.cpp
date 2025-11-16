32b62210f2fc1bd3bbdd9b7b0247f8451a36dd9c
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:27:19 -0600

    Update logger.cpp
    Add file description

--- avionics/firmware/src/drivers/logger.cpp
+++ avionics/firmware/src/drivers/logger.cpp
@@ -1,1 +1,9 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Drivers: Logger
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Provides simple logging to UART for now, later to SD card if desired. Logs to include time, attitude, RC inputs, control outputs, and flight mode.
+// Logs to be in CSV-friendly format for plotting in Python and/or MATLAB.
