bd86981d8a9f33de7d4e6e9050ca6e2c5de08ddb
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:33:32 -0600

    Update mixer.cpp
    Add file description

--- avionics/firmware/src/control/mixer.cpp
+++ avionics/firmware/src/control/mixer.cpp
@@ -1,1 +1,10 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Control: Mixer
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Takes in the control outputs (rollCMD, pitchCMD, yawCMD, throttle) and converts to the following individual surface deflections:
+//    Aileron Left/Right, Elevator, Rudder, Throttle (ESC)
+// Shall handle different mixing schemes, i.e., a V-tail, if implemented
