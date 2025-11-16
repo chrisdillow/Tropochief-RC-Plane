079d2e97e92e6568df92afd18c10b7c9b6eeafad
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:33:47 -0600

    Update adcSticks.cpp
    Add file description

--- controller/firmware/src/drivers/adcSticks.cpp
+++ controller/firmware/src/drivers/adcSticks.cpp
@@ -1,1 +1,8 @@
+// Tropochief Project
+// Chris Dillow
+// Controller Firmware Drivers: ADC Sticks
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Reading of the analog gimbal axes.
