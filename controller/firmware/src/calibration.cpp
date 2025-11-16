851194ee6e09d72c87b1dd0e04c8a942b2a11e22
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:30:13 -0600

    Update calibration.cpp
    Add file description

--- controller/firmware/src/calibration.cpp
+++ controller/firmware/src/calibration.cpp
@@ -1,1 +1,8 @@
+// Tropochief Project
+// Chris Dillow
+// Controller Firmware: Calibration
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Map raw ADC values to normalized [-1.0,+1.0], plus deadzones
