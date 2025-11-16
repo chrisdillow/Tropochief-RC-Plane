a8dc5f103f2b75ce0312d8f269047b2bc5f9f3de
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:18:59 -0600

    Update rcProtocol.cpp
    Add file description

--- avionics/firmware/src/rc/rcProtocol.cpp
+++ avionics/firmware/src/rc/rcProtocol.cpp
@@ -1,1 +1,9 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware RC: Protocol
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Parse RF packets into normalized channel values [-1.0,+1.0], validate checksum, and handle signal loss detection when no packet has been received
+// in a predetermined count of milliseconds.
