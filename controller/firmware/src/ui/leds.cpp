83356d4a1b02d12873a53bb9eb5925e77278ae41
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-16 00:24:38 -0600

    Update leds.cpp
    Add file description

--- controller/firmware/src/ui/leds.cpp
+++ controller/firmware/src/ui/leds.cpp
@@ -1,1 +1,8 @@
+// Tropochief Project
+// Chris Dillow
+// Controls Firmware UI: LEDs
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Operate LEDs to show link state, flight mode, and a low battery warning.
