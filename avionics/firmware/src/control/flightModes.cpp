a8eb5084162bf2e07d75665f873b13648f57d708
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:36:39 -0600

    Update flightModes.cpp
    Add file description

--- avionics/firmware/src/control/flightModes.cpp
+++ avionics/firmware/src/control/flightModes.cpp
@@ -1,1 +1,12 @@
 
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Control: Flight Modes
+
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Implements logic for manual and stabilized flight modes.
+//    MANUAL: RC directly to surfaces within specified limits.
+//    STABILIZED: RC sets attitude setpoints, while inner loop stabilizes.
+// OPTIONAL: Future implementation of attitude or heading holding.
