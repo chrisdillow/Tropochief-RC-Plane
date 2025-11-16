3cfa742b2ba916c6284425900781785105207dd8
Caressa (Chris) Dillow <161703287+chrisdillow@users.noreply.github.com>
2025-11-15 21:28:47 -0600

    Update rfLink.cpp
    Add file description

--- avionics/firmware/src/drivers/rfLink.cpp
+++ avionics/firmware/src/drivers/rfLink.cpp
@@ -1,1 +1,9 @@
+// Tropochief Project
+// Chris Dillow
+// Avionics Firmware Drivers: RF Link
 
+// ======================================== //
+// |           FILE DESCRIPTION           | //
+// ======================================== //
+// Wraps the RF driver APIs rfInit(), rfReceivePacket(), etc.
+// Must work with whichever 2.4 GHz module is chosen.
